cc0a880ed2e9bb3b45427e53b046d1b67ab8e27e
AtharvaArgade <atharvaargade@gmail.com>
2023-04-13 11:40:59 +0530

    Third commit

--- add/Demo.java
+++ add/Demo.java
@@ -1,10 +1,17 @@
 class Demo{
 
 	void add(){
-		int i = 5;
-		int j = 4;
-		int z = i+j;
-		System.out.printlv("Sum is : " + z);
+		int a = 5;
+		int b = 4;
+		int c = i+j;
+		System.out.printlv("Sum is : " + c);
+	}
+
+	void div(){
+		int i = 12;
+		itn j = 2;
+		int z = i/j;
+		System.out.println("Div is : "+ z);
 	}
 	public static void main(String arg[]){
 
