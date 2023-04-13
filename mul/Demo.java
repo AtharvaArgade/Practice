4baab5d212a7de7a8c1d5ceeed0aa3901a6cf637
AtharvaArgade <atharvaargade@gmail.com>
2023-04-13 11:47:42 +0530

    fifth commit

--- mul/Demo.java
+++ mul/Demo.java
@@ -5,6 +5,10 @@ class Demo{
 		int j = 4;
 		int z = i*j;
 		System.out.printlv("Sum is : " + z);
+		 i = 5;
+		 j = 4;
+		 z = i*j;
+		System.out.printlv("Sum is : " + z);
 	}
 	public static void main(String arg[]){
 
